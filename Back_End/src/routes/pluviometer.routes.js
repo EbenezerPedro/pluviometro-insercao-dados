74b67b42e1eedbf190c39ae2ad8441833b20ab18
ALEXIS FERNANDO SOLIS MAMANI <145046699+AlexisSolis7@users.noreply.github.com>
2024-12-08 22:32:33 -0300

    Delete Back_End directory

--- Back_End/src/routes/pluviometer.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-
-const { renderPluviometerForm, createPluviometerData } = require('../controllers/pluviometer.controller');
-
-router.get('/pluviometer/new', renderPluviometerForm);
-router.post('/pluviometer/new', createPluviometerData);
-
-module.exports = router;
